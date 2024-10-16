1d5131e01cef84b488d87eaae1f80c9a2fd4d08e
manleofer <mleofer304@iesalmudeyne.es>
2024-10-16 09:16:23 +0200

    Version inicial del fichero HolaMundo.java sin colores

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+/**
+ * Muestra por pantalla la frase "¡Hola mundo!"
+ */
+public class HolaMundo{
+    public static void main (String[] args){
+        System.out.println("¡Hola mundo!");
+        System.out.println ("Aquí estamos haciendo pruebas en Git.");
+    }
+}
